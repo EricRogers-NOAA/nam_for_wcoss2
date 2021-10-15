448c45e0c197005b8de91aa2c3692acecc27fe92
Eric Rogers <eric.rogers@noaa.gov>
2021-10-15 15:56:55 -0400

    Update tm01 submission scripts

--- ecf/catchup/tm01/run_nam_catchup_forecast_tm01.sh
+++ ecf/catchup/tm01/run_nam_catchup_forecast_tm01.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-#PBS -N nam_catchup_analysis_conus_tm01_12
-#PBS -l place=vscatter:excl,select=11:ncpus=128:mpiprocs=16:ompthreads=8
-#PBS -l walltime=00:20:00
-#PBS -e /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_catchup_analysis_conus_tm01_12.out
-#PBS -o /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_catchup_analysis_conus_tm01_12.out
+#PBS -N nam_catchup_forecast_tm01_12
+#PBS -l place=vscatter:excl,select=14:ncpus=128
+#PBS -l walltime=00:45:00
+#PBS -e /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_catchup_forecast_tm01_12.out
+#PBS -o /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_catchup_forecast_tm01_12.out
 #PBS -q dev
 #PBS -A NAM-DEV
 #PBS -l debug=true
 #PBS -V
+
+set -x
 
 module purge
 module load envvar/1.0
@@ -17,19 +19,21 @@ module load intel/19.1.3.304
 module load cray-mpich/8.1.9
 module load cray-pals/1.0.17
 
-module load prod_util/2.0.10
-module load prod_envir/2.0.5
-module load crtm/2.3.0
+module load prod_util/2.0.9
+module load prod_envir/2.0.4
 module load cfp/2.0.4
+module load hdf5/1.10.6
 module load netcdf/4.7.4
+module load nemsio/2.5.2
+module load libpng/1.6.37
+module load libjpeg/9c
+module load grib_util/1.2.3
 
 set -x
 
-export FI_OFI_RXM_SAR_LIMIT=3145728
-
-export ntasks=176
-export ppn=16
-export threads=8
+export ntasks=1792
+export ppn=128
+export threads=1
 
 # OMP settings
 export OMP_PLACES=cores
@@ -40,9 +44,8 @@ export cyc=12
 export PDY=20210824
 export tmmark=tm01
 export envir=canned
-export domain=conus
 export nam_ver=v4.2.0
-export jobid=jnam_catchup_analysis_${domain}.${tmmark}_${cyc}.${PBS_JOBID}
+export jobid=jnam_catchup_forecast_${tmmark}_${cyc}.${PBS_JOBID}
 export NWROOT=/lfs/h2/emc/lam/noscrub/Eric.Rogers
 export PACKAGEROOT=/lfs/h2/emc/lam/noscrub/Eric.Rogers
 
@@ -55,11 +58,4 @@ export HOMEjobs=/lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/jobs
 
 #execute J-job
 
-$HOMEjobs/JNAM_ANALYSIS_NEST
-
-
-
-
-
-
-
+$HOMEjobs/JNAM_FORECAST
