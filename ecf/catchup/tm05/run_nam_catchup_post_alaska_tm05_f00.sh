5330e50b7a56e7e866e1f8051edb2dc6cee851b5
Eric Rogers <eric.rogers@noaa.gov>
2021-10-15 12:11:24 -0400

    Update tm05 catchup submission scripts

--- ecf/catchup/tm05/run_nam_catchup_post_alaska_tm05_f00.sh
+++ ecf/catchup/tm05/run_nam_catchup_post_alaska_tm05_f00.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #PBS -N nam_nam_catchup_post_alaska_tm05_f00_12
-#PBS -l select=1:ncpus=28:mem=28GB
+#PBS -l select=1:ncpus=28:mem=50GB
 #PBS -l walltime=00:20:00
 #PBS -e /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_catchup_post_alaska_tm05_f00_12
 #PBS -o /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_catchup_post_alaska_tm05_f00_12
@@ -9,18 +9,16 @@
 #PBS -l debug=true
 #PBS -V
 
-set -x
-
 module purge
 module load envvar/1.0
 module load intel/19.1.3.304
 module load PrgEnv-intel/8.1.0
-module load craype/2.7.6
-module load cray-mpich/8.1.7
-module load cray-pals/1.0.12
+module load craype/2.7.10
+module load cray-mpich/8.1.9
+module load cray-pals/1.0.17
 module load hdf5/1.10.6
 module load netcdf/4.7.4
-module load prod_util/2.0.9
+module load prod_util/2.0.10
 module load crtm/2.3.0
 
 set -x
