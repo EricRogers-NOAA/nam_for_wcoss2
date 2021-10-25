6590c27c5c513f072fc7db6c62dce32a0673c4c1
Eric Rogers <eric.rogers@noaa.gov>
2021-10-25 10:42:02 -0400

    Update submission scripts

--- ecf/main_bocos/run_nam_metgrid_boco.sh
+++ ecf/main_bocos/run_nam_metgrid_boco.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#PBS -N nam_coldstart_metgrid_boco_12
+#PBS -N nam_metgrid_boco_18
 #PBS -l select=1:ncpus=15:mem=30GB
 #PBS -l walltime=00:20:00
-#PBS -e /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_metgrid_boco_12.out
-#PBS -o /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_metgrid_boco_12.out
+#PBS -e /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_metgrid_boco_18.out
+#PBS -o /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_metgrid_boco_18.out
 #PBS -q dev
 #PBS -A NAM-DEV
 #PBS -l debug=true
@@ -13,20 +13,20 @@ module purge
 module load envvar/1.0
 module load PrgEnv-intel/8.1.0
 module load intel/19.1.3.304
-module load craype/2.7.6
-module load cray-mpich/8.1.7
-module load cray-pals/1.0.12
+module load cray-pals/1.0.17
+module load craype/2.7.10
+module load cray-mpich/8.1.9
 
 # Loading NetCDF 4
 module load netcdf/4.7.4
-module load prod_util/2.0.9
+module load prod_util/2.0.10
 module load prod_envir/2.0.5
 module load libjpeg/9c
-module load grib_util/1.2.2
+module load grib_util/1.2.3
 
 set -x
 
-export cyc=12
+export cyc=18
 export PDY=20210824
 export tmmark=tm00
 export envir=canned
--- ecf/main_bocos/run_nam_prelim.sh
+++ ecf/main_bocos/run_nam_prelim.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#PBS -N nam_main.out_12
+#PBS -N nam_main.out_18
 #PBS -l select=1:ncpus=1:mem=2GB
 #PBS -l walltime=00:20:00
-#PBS -e /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_main_12.out
-#PBS -o /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_main_12.out
+#PBS -e /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_main_18.out
+#PBS -o /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_main_18.out
 #PBS -q dev
 #PBS -A NAM-DEV
 #PBS -l debug=true
@@ -13,19 +13,19 @@ module purge
 module load envvar/1.0
 module load PrgEnv-intel/8.1.0
 module load intel/19.1.3.304
-module load craype/2.7.6
-module load cray-mpich/8.1.7
+module load craype/2.7.10
+module load cray-mpich/8.1.9
 
 # Loading NetCDF 4
 module load netcdf/4.7.4
-module load prod_util/2.0.9
+module load prod_util/2.0.10
 module load prod_envir/2.0.5
 module load libjpeg/9c
-module load grib_util/1.2.2
+module load grib_util/1.2.3
 
 set -x
 
-export cyc=12
+export cyc=18
 export PDY=20210824
 export tmmark=tm00
 export envir=canned
