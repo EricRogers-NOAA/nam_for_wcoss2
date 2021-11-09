57c83c8a6689775e62d7dd9050308b222becf82b
Eric Rogers <eric.rogers@noaa.gov>
2021-11-09 17:49:37 -0500

    Add post manager submission scripts

--- ecf/post/run_nam_post_conus_manager.sh
+++ ecf/post/run_nam_post_conus_manager.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#PBS -N nam_post_manager_12
+#PBS -N nam_post_conus_manager_12
 #PBS -l select=1:ncpus=1:mem=200MB
 #PBS -l walltime=02:20:00
-#PBS -e /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_post_manager_12.out
-#PBS -o /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_post_manager_12.out
+#PBS -e /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_post_conus_manager_12.out
+#PBS -o /lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/logs/nam_post_conus_manager_12.out
 #PBS -q dev
 #PBS -A NAM-DEV
 #PBS -l debug=true
@@ -31,9 +31,10 @@ set -x
 export cyc=12
 export PDY=20210824
 export tmmark=tm00
+export domain=conus
 export envir=canned
 export nam_ver=v4.2.0
-export jobid=jnam_post_manager_${cyc}.${PBS_JOBID}
+export jobid=jnam_post_conus_manager_${cyc}.${PBS_JOBID}
 
 export NWROOT=/lfs/h2/emc/lam/noscrub/Eric.Rogers
 export PACKAGEROOT=/lfs/h2/emc/lam/noscrub/Eric.Rogers
@@ -48,4 +49,4 @@ export HOMEjobs=/lfs/h2/emc/lam/noscrub/Eric.Rogers/nam.v4.2.0/jobs
 
 #execute J-job
 
-$HOMEjobs/JNAM_POST_MANAGER
+$HOMEjobs/JNAM_POST_MANAGER_NEST
