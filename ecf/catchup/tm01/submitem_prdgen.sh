45372741d45d985d03cf787e40e19aa4c2e3786a
EricRogers-NOAA <48292889+EricRogers-NOAA@users.noreply.github.com>
2021-11-17 10:02:47 -0500

    Delete submitem_prdgen.sh

--- ecf/catchup/tm01/submitem_prdgen.sh
+++
@@ -1,8 +0,0 @@
-set -x
-
-qsub run_nam_catchup_prdgen_alaska_tm01_f00.sh
-qsub run_nam_catchup_prdgen_alaska_tm01_f01.sh
-qsub run_nam_catchup_prdgen_conus_tm01_f00.sh
-qsub run_nam_catchup_prdgen_conus_tm01_f01.sh
-qsub run_nam_catchup_prdgen_tm01_f00.sh
-qsub run_nam_catchup_prdgen_tm01_f01.sh
